2f8ce468c919990206b0b16fece3422ef4d6ee17
marinaerokhina <erokhina.marina.2016@mail.ru>
2021-11-20 21:35:40 +0700

    rtr

--- app/src/main/java/com/example/ecology/TestChoiceActivity.java
+++ app/src/main/java/com/example/ecology/TestChoiceActivity.java
@@ -1,6 +1,8 @@
 package com.example.ecology;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TestChoiceActivity extends AppCompatActivity {
@@ -8,5 +10,17 @@ public class TestChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_testchoice);
+    }
+
+    public void onGoingBack(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        onDestroy();
+    }
+
+    public void test_1 (View view){
+        Intent intent = new Intent(this, Test_1.class);
+        startActivity(intent);
+        onPause();
     }
 }
---
+++ app/src/main/java/com/example/ecology/Test_1.java
@@ -0,0 +1,6 @@
+package com.example.ecology;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Test_1 extends AppCompatActivity {
+}
