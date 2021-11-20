7a46a708334bb6e32e353f9275039c3165b936e5
marinaerokhina <erokhina.marina.2016@mail.ru>
2021-11-20 21:43:36 +0700

    rtr

--- app/src/main/java/com/example/ecology/Theme_1.java
+++ app/src/main/java/com/example/ecology/Theme_1.java
@@ -1,7 +1,13 @@
 package com.example.ecology;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Theme_1  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme1);
+    }
 
 }
