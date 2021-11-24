e2e7ae9f8403dab8c3a697afb758f9b22c1ce2c8
marinaerokhina <erokhina.marina.2016@mail.ru>
2021-11-24 16:50:18 +0700

    rtr

--- app/src/main/java/com/example/ecology/ChoiceActivity.java
+++ app/src/main/java/com/example/ecology/ChoiceActivity.java
@@ -3,6 +3,7 @@ package com.example.ecology;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ChoiceActivity extends AppCompatActivity {
@@ -21,45 +22,54 @@ public class ChoiceActivity extends AppCompatActivity {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_3 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_4 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_5 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_6 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_7 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_8 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_9 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
     public void theme_10 (View view) {
         /*Intent intent = new Intent(this, Theme_1.class);
         startActivity(intent);
         onPause();*/
+        Toast.makeText(this, "@string/unavailable_func", Toast.LENGTH_LONG).show();
     }
 }
