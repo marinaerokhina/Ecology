9864385038df765b5f90c1e5c27197f1cc452d30
marinaerokhina <erokhina.marina.2016@mail.ru>
2021-11-26 23:41:11 +0700

    rtr

--- app/src/main/java/com/example/ecology/Test_1.java
+++ app/src/main/java/com/example/ecology/Test_1.java
@@ -15,6 +15,7 @@ public class Test_1 extends AppCompatActivity {
     String[] a3 = new String[10];
     String[] a4 = new String[10];
     String[] r_a = new String[10];
+    boolean[] an = new boolean[10];
     int cnt=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +26,85 @@ public class Test_1 extends AppCompatActivity {
         ans3=findViewById(R.id.btn_ans3);
         ans4=findViewById(R.id.btn_ans4);
         quest=findViewById(R.id.question);
-
+        q[0]="Кто написал письмо Фуску Авдию?";
+        a1[0]="Птолемей";
+        a2[0]="Аристотель";
+        a3[0]="Гораций";
+        a4[0]="Цезарь";
+        r_a[0]="Гораций";
+        quest.setText(q[0]);
+        ans1.setText(a1[0]);
+        ans2.setText(a2[0]);
+        ans3.setText(a3[0]);
+        ans4.setText(a4[0]);
     }
 
-    public void onTesting(View view){
-
+    public void onTesting1(View view){
+        if(r_a[cnt].contentEquals(ans1.getText())){
+            an[cnt]=true;
+        }
+        else an[cnt]=false;
+        cnt++;
+        if(cnt<10){
+            quest.setText(q[cnt]);
+            ans1.setText(a1[cnt]);
+            ans2.setText(a2[cnt]);
+            ans3.setText(a3[cnt]);
+            ans4.setText(a4[cnt]);
+        }
+        else {
+            /**/
+        }
+    }
+    public void onTesting2(View view){
+        if(r_a[cnt].contentEquals(ans2.getText())){
+            an[cnt]=true;
+        }
+        else an[cnt]=false;
+        cnt++;
+        if(cnt<10){
+            quest.setText(q[cnt]);
+            ans1.setText(a1[cnt]);
+            ans2.setText(a2[cnt]);
+            ans3.setText(a3[cnt]);
+            ans4.setText(a4[cnt]);
+        }
+        else {
+            /**/
+        }
+    }
+    public void onTesting3(View view){
+        if(r_a[cnt].contentEquals(ans3.getText())){
+            an[cnt]=true;
+        }
+        else an[cnt]=false;
+        cnt++;
+        if(cnt<10){
+            quest.setText(q[cnt]);
+            ans1.setText(a1[cnt]);
+            ans2.setText(a2[cnt]);
+            ans3.setText(a3[cnt]);
+            ans4.setText(a4[cnt]);
+        }
+        else {
+            /**/
+        }
+    }
+    public void onTesting4(View view){
+        if(r_a[cnt].contentEquals(ans4.getText())){
+            an[cnt]=true;
+        }
+        else an[cnt]=false;
+        cnt++;
+        if(cnt<10){
+            quest.setText(q[cnt]);
+            ans1.setText(a1[cnt]);
+            ans2.setText(a2[cnt]);
+            ans3.setText(a3[cnt]);
+            ans4.setText(a4[cnt]);
+        }
+        else {
+            /**/
+        }
     }
 }
